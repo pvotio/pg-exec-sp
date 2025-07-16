43f1002ffcc1178217e6ed300b09f9c06d312434
Clem Struck <69113110+clemstruck@users.noreply.github.com>
2025-07-16 10:39:03 +0200

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -3,6 +3,7 @@ from typing import Sequence
 
 import psycopg
 from decouple import config
+from azure.identity import DefaultAzureCredential  # New import
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +16,24 @@ class PostgreSQLDatabase:
         self.database: str = config("PG_DATABASE", default="postgres")
         self.username: str = config("PG_USERNAME", default="postgres")
         self.password: str = config("PG_PASSWORD", default="")
-        self.sslmode: str = config("PG_SSL_MODE", default="prefer")
+        self.sslmode: str = config("PG_SSL_MODE", default="require")  # Changed default to 'require' for Azure
+        self.auth_method: str = config("PG_AUTH_METHOD", default="password")  # New: 'password' or 'entra'
         self.connection = self._connect()
 
     def _connect(self):
+        if self.auth_method.lower() == "entra":
+            # Use DefaultAzureCredential for Entra ID token
+            credential = DefaultAzureCredential()
+            token = credential.get_token("https://ossrdbms-aad.database.windows.net/.default").token
+            password = token  # Token acts as password
+            logger.info("Using Microsoft Entra ID authentication with DefaultAzureCredential.")
+        else:
+            password = self.password
+            logger.info("Using password-based authentication.")
+
         dsn = (
             f"host={self.host} port={self.port} dbname={self.database} "
-            f"user={self.username} password={self.password} sslmode={self.sslmode}"
+            f"user={self.username} password={password} sslmode={self.sslmode}"
         )
         logger.info("Connecting to PostgreSQL at %s:%s/%s", self.host, self.port, self.database)
         conn = psycopg.connect(dsn, autocommit=True)
